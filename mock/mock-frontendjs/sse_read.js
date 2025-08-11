4ef7eab358f3a8856a0dbd4ae4b3f48e3a9624ee
vinayhr01 <vinayhr1211@gmail.com>
2025-08-11 15:26:46 +0530

    changes

--- mock/mock-frontendjs/sse_read.js
+++ mock/mock-frontendjs/sse_read.js
@@ -1,10 +1,13 @@
-const jobId = "68919bbdc77185fb9ed98f66"; // replace with actual jobId
-const es = new EventSource(`http://localhost:3000/api/updates/${jobId}`);
+const jobId = "6899b4a4bedcc774dc6eb90c"; // replace with actual jobId
+const es = new EventSource(`http://13.212.112.213:3000/api/updates/${jobId}`);
 
 es.onmessage = (event) => {
   console.log("Update received:", JSON.parse(event.data));
 };
 
 es.onerror = (err) => {
+  console.error("SSE Error:", err);
+};
+(err) => {
   console.error("SSE Error:", err);
 };
