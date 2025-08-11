0fe90724862e0c7e137d7fbb76f4ffe66d67fcea
vinayhr01 <vinayhr1211@gmail.com>
2025-08-11 13:10:44 +0530

    revert change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', routes);
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).send({ status: 'OK', message: 'Healthy' });
 });
 
